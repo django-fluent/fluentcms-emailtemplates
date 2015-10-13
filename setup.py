ed83f34f631120efa4fbbb7f33e2b4f26b4c486e
Diederik van der Boor <vdboor@edoburu.nl>
2015-10-13 11:29:26 +0200

    Fix compilemessages command in setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,11 @@ import sys
 if 'sdist' in sys.argv or 'develop' in sys.argv:
     os.chdir('fluentcms_emailtemplates')
     try:
-        from django.core.management.commands.compilemessages import Command
-        Command().execute(stdout=sys.stderr, exclude=(), verbosity=1)
+        from django.core import management
+        management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
     except ImportError:
-        # < Django 1.7
-        from django.core.management.commands.compilemessages import compile_messages
-        compile_messages(sys.stderr)
+        if 'sdist' in sys.argv:
+            raise
     finally:
         os.chdir('..')
 
