573479c7e99c00557bc842792037010398fac26a
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-22 14:55:18 +0100

    fixed package installs

--- runtests.py
+++ runtests.py
@@ -3,6 +3,14 @@ import sys
 import django
 from django.conf import settings, global_settings as default_settings
 from django.core.management import execute_from_command_line
+from os import path
+
+# Give feedback on used versions
+sys.stderr.write('Using Python version {0} from {1}\n'.format(sys.version[:5], sys.executable))
+sys.stderr.write('Using Django version {0} from {1}\n'.format(
+    django.get_version(),
+    path.dirname(path.abspath(django.__file__)))
+)
 
 if not settings.configured:
     settings.configure(
